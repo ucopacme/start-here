1a553f979eab1fd11a6bdffafb92b6b3b9eae917
EC2 Default User <ec2-user@ip-172-31-8-183.us-west-2.compute.internal>
2018-10-07 17:23:04 +0000

    hands_on_labs: example solution for sns

--- hands_on_lab/sample_solutions/boto3-create-sns-topic.py
+++ hands_on_lab/sample_solutions/boto3-create-sns-topic.py
@@ -1,10 +1,21 @@
 #!/bin/env python 
+"""
+Create or delete a named SNS topic.
+
+Usage: ./boto3-create-sns-topic.py <action> <topic_name>
+  where "action" is one of "create", "report" or "delete"
+
+Examples:
+  ./boto3-create-sns-topic.py create ashtest-01
+  ./boto3-create-sns-topic.py report ashtest-01
+  ./boto3-create-sns-topic.py delete ashtest-01
+
+"""
+
+
 import sys
 import boto3 
 import yaml
-
-
-#TOPIC_NAME=ashtest-01
 
 
 def usage():
@@ -26,8 +37,19 @@ def create_sns_topic(topic_name):
 def get_topic_arn(topic_name):
     sns_client = boto3.client('sns')
     response = sns_client.list_topics()
-    topic_arn = [arn['TopicArn'] for arn in response['Topics'] if arn['TopicArn'].split(':')[-1] == topic_name][0]
-    return topic_arn
+    topic_arn = (
+        arn['TopicArn'] for arn in response['Topics'] 
+        if arn['TopicArn'].split(':')[-1] == topic_name
+    )
+    return next(topic_arn, None)
+
+
+def delete_sns_topic(topic_name):
+    sns_client = boto3.client('sns')
+    topic_arn = get_topic_arn(topic_name)
+    if topic_arn is not None:
+        print('deleting {}'.format(topic_arn))
+        sns_client.delete_topic(TopicArn=topic_arn)
 
                 
 if __name__ == '__main__':
@@ -42,11 +64,10 @@ if __name__ == '__main__':
         print(yamlfmt(create_sns_topic(topic_name)))
 
     elif action == 'report':
-        topic_arn = get_topic_arn(topic_name)
-        print(topic_arn)
+        print(get_topic_arn(topic_name))
 
     elif action == 'delete':
-        print(action)
+        delete_sns_topic(topic_name)
 
     else:
         sys.exit(usage())
