22adef6adf8249cbe7fbceaa149540197a7601ba
David Rivera <David.Rivera@ucop.edu>
2018-10-14 09:12:17 -0700

    update lab1 based off comments given

--- hands_on_lab/user_submissions/davidjrivera/lab1/lab1-add.py
+++ hands_on_lab/user_submissions/davidjrivera/lab1/lab1-add.py
@@ -1,57 +1,47 @@
-#!/home/drivera/python/python36/bin/python3
+#!/bin/env python
+
+#
+# Usage: ./lab1-add-01.py
+#
 
 import boto3
+import yaml
 
-s3 = boto3.client('s3')
 
-def creates3():
- #Create s3 Bucket
- s3bucket =  s3.create_bucket(Bucket='lab1davidpython', CreateBucketConfiguration={
-	'LocationConstraint': 'us-west-2'})
- print('')
- print('S3 Bucket being created')
- print('----------------------------')
- print(s3bucket)
-creates3()
+def yamlfmt(obj):
+    if isinstance(obj, str):
+        return obj
+    return yaml.dump(obj, default_flow_style=False)
 
-def listbucket():
-  response = s3.list_buckets()
-  buckets = [bucket['Name'] for bucket in response['Buckets']]
-
-listbucket()
-# Upload file from CWD to the new s3 bucket
-
-def uploadfile():
+def creates3(new_buck):
+#Create s3 Bucket
+     s3 = boto3.client('s3')
+     response =  s3.create_bucket(Bucket=new_buck, CreateBucketConfiguration={'LocationConstraint': 'us-west-2'})
+     print(response)
+     return response
  
- filexfer =  s3.upload_file('lab1daviddoc.txt', 'lab1davidpython', 'remotefile.txt')
-# print(filexfer) 
-uploadfile()
-# 
 
-def lists3obj():
-  response = s3.list_objects(
-      Bucket='lab1davidpython'
-	  )
-  print()
-  print()
-#  print("s3 Bucket Name: %s" % response.get('Name'))
-  print()
-  print()
-  print(' Data Transfered to S3 Bucket')
-  print('-----------------------------------------------')
-  print("Makeup of s3 Bucket: %s" % response.get('Contents'))
-lists3obj()
-def dels3():  
-  s3 = boto3.resource('s3')
-  b = s3.Bucket('lab1davidpython')
-  b.objects.filter(Prefix='remotefile.txt').delete()
-  s3 = boto3.client('s3')
-  delbuck = s3.delete_bucket(
-  Bucket='lab1davidpython'
-   )
-  print()
-  print()
-  print(' Delete S3 Bucket')
-  print('---------------------')
-  print(delbuck)
-dels3()
+def dels3(new_buck):
+#Delete s3 Bucket
+      s3 = boto3.client('s3')
+      delbuck = s3.delete_bucket(Bucket=new_buck)
+      print(delbuck)
+      return delbuck
+
+def main():
+    mybuck = 'lab1davidpythons3'
+    result = creates3(mybuck)
+    print('S3 Bucket being created')
+    print('----------------------------')
+    print(yamlfmt(result['Location']))
+
+    mybuck = 'lab1davidpythons3'
+    result = dels3(mybuck)
+    print(' Delete S3 Bucket')
+    print('---------------------')
+    
+
+
+
+if __name__ == "__main__":
+    main()
--- hands_on_lab/user_submissions/davidjrivera/scratch/lab1-add.py
+++ hands_on_lab/user_submissions/davidjrivera/scratch/lab1-add.py
@@ -1,33 +1,57 @@
 #!/home/drivera/python/python36/bin/python3
 
 import boto3
-# Create s3 Bucket
+
 s3 = boto3.client('s3')
-s3.create_bucket(Bucket='lab1davidpython', CreateBucketConfiguration={
+
+def creates3():
+ #Create s3 Bucket
+ s3bucket =  s3.create_bucket(Bucket='lab1davidpython', CreateBucketConfiguration={
 	'LocationConstraint': 'us-west-2'})
+ print('')
+ print('S3 Bucket being created')
+ print('----------------------------')
+ print(s3bucket)
+creates3()
 
-response = s3.list_buckets()
-buckets = [bucket['Name'] for bucket in response['Buckets']]
+def listbucket():
+  response = s3.list_buckets()
+  buckets = [bucket['Name'] for bucket in response['Buckets']]
 
+listbucket()
 # Upload file from CWD to the new s3 bucket
-s3.upload_file('lab1daviddoc.txt', 'lab1davidpython', 'remotefile.txt')
 
+def uploadfile():
+ 
+ filexfer =  s3.upload_file('lab1daviddoc.txt', 'lab1davidpython', 'remotefile.txt')
+# print(filexfer) 
+uploadfile()
 # 
-response = s3.list_objects(
-    Bucket='lab1davidpython'
-	)
 
-print()
-print()
-print("s3 Bucket Name: %s" % response.get('Name'))
-print()
-print()
-print("Makeup of s3 Bucket: %s" % response.get('Contents'))
-s3 = boto3.resource('s3')
-b = s3.Bucket('lab1davidpython')
-b.objects.filter(Prefix='remotefile.txt').delete()
-print("Bucket List: %s" % buckets)
-s3 = boto3.client('s3')
-response = s3.delete_bucket(
-   Bucket='lab1davidpython'
-)
+def lists3obj():
+  response = s3.list_objects(
+      Bucket='lab1davidpython'
+	  )
+  print()
+  print()
+#  print("s3 Bucket Name: %s" % response.get('Name'))
+  print()
+  print()
+  print(' Data Transfered to S3 Bucket')
+  print('-----------------------------------------------')
+  print("Makeup of s3 Bucket: %s" % response.get('Contents'))
+lists3obj()
+def dels3():  
+  s3 = boto3.resource('s3')
+  b = s3.Bucket('lab1davidpython')
+  b.objects.filter(Prefix='remotefile.txt').delete()
+  s3 = boto3.client('s3')
+  delbuck = s3.delete_bucket(
+  Bucket='lab1davidpython'
+   )
+  print()
+  print()
+  print(' Delete S3 Bucket')
+  print('---------------------')
+  print(delbuck)
+dels3()
