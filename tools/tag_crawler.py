cf4778560d71340cb8be886345009afe02e59d32
Ashley Gould <agould@ucop.edu>
2018-10-20 05:21:46 -0700

    tools tag_crawler: add purge_empty_responses()

--- tools/tag_crawler.py
+++ tools/tag_crawler.py
@@ -24,21 +24,22 @@ def get_tags(region, account):
         region_name=region, 
         **account.credentials,
     )
-    filters = dict(
-        TagFilters=[
-            #dict(Key='Name'),
-            #dict(Key='24hourssh'),
-            #dict(Key='aws:cloudformation:stack-name'),
-        ],
-        ResourceTypeFilters=['ec2:instance'],
-    )
+    filters = dict()
+    #filters = dict(
+    #    TagFilters=[
+    #        dict(Key='Name'),
+    #        #dict(Key='24hourssh'),
+    #        #dict(Key='aws:cloudformation:stack-name'),
+    #    ],
+    #    ResourceTypeFilters=['ec2:instance'],
+    #)
     response = client.get_resources(
-        #**filters,
+        **filters,
     )
     tag_mapping_list = response['ResourceTagMappingList']
     while response['PaginationToken']:
         response = client.get_resources(
-            #**filters,
+            **filters,
             PaginationToken=response['PaginationToken'],
         )
         tag_mapping_list += response['ResourceTagMappingList']
@@ -52,24 +53,31 @@ def get_crawler(org_access_role):
     my_crawler = crawlers.Crawler(
         my_org,
         access_role=org_access_role,
-        #accounts=test_accounts,
-        accounts=['ait-poc', 'ashley-training'],
-        regions=['us-west-2', 'us-east-1'],
+        #accounts=['ait-poc', 'ashley-training'],
+        #regions=['us-west-2', 'us-east-1'],
     )
     my_crawler.load_account_credentials()
     return my_crawler
 
 
-def output_regions_per_account(execution):
-    """ generate dictionary of responses per account """
-    # purge empty responses
+def purge_empty_responses(execution):
+    '''
+    Return list of execution responses for which output is not empty.
+    Expects each response to be a list of dict.
+    '''
     responses = [
         r for r in execution.responses 
-        if r.payload_output['ResourceTagMappingList']
+        if len(r.payload_output) == 1
+        and list() not in r.payload_output.values()
     ]
-    # list of account names unique and sorted
-    account_names = sorted(list(set([r.account.name for r in responses])))
+    return responses
+
+
+def output_regions_per_account(execution):
+    """ generate dictionary of responses per account """
     collector = []
+    responses = purge_empty_responses(execution)
+    account_names = sorted(list(set([r.account.name for r in responses])))
     for account_name in account_names:
         d = dict(
             Account=account_name,
@@ -79,7 +87,7 @@ def output_regions_per_account(execution):
             ]
         )
         collector.append(d)
-    return(utils.yamlfmt(collector))
+    return(collector)
 
 
 def main():
@@ -87,7 +95,7 @@ def main():
     crawler = get_crawler(org_access_role)
     execution = crawler.execute(get_tags)
     output = output_regions_per_account(execution)
-    print(output)
+    print(utils.yamlfmt(output))
 
 
 if __name__ == "__main__":
