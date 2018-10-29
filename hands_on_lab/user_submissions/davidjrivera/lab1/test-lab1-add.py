c2393b78d001446fb30ef8695dc7bfb8605314c6
David Rivera <David.Rivera@ucop.edu>
2018-10-29 10:57:19 -0700

    updated files for moto on lab1

--- hands_on_lab/user_submissions/davidjrivera/lab1/test-lab1-add.py
+++ hands_on_lab/user_submissions/davidjrivera/lab1/test-lab1-add.py
@@ -24,9 +24,10 @@ def test_creates3():
      s3 = boto3.client('s3')
      new_bucket = 'lab1davidpythons3'
      response =  s3.create_bucket( Bucket=new_bucket,  CreateBucketConfiguration={'LocationConstraint': 'us-west-2'})
-     test = (response['Location'])
-     assert test == 'http://lab1davidpythons3.s3.amazonaws.com/'
-     return response
+     #test = (response['Location'])
+     #assert test == 'http://lab1davidpythons3.s3.amazonaws.com/'
+     assert response['ResponseMetadata']['HTTPStatusCode'] == 200
+     #return response
  
 @mock_s3
 def test_dels3():
@@ -34,6 +35,7 @@ def test_dels3():
       new_bucket = 'lab1davidpythons3'
       response =  s3.create_bucket( Bucket=new_bucket,  CreateBucketConfiguration={'LocationConstraint': 'us-west-2'})
       delbuck = s3.delete_bucket(Bucket=new_bucket)
-      print(delbuck)
-      return delbuck
+      assert response['ResponseMetadata']['HTTPStatusCode'] == 404
+   #   print(delbuck)
+      #return delbuck
 
