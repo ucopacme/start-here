17155a525632a1ae45d72c1097c34fb864574d6e
JinJya Hsu <jhsu@ucop.edu>
2018-10-30 15:28:02 -0700

    update lab_2_def.py test_lab_2.py

--- hands_on_lab/user_submissions/jhsu/lab_2/test_lab_2.py
+++ hands_on_lab/user_submissions/jhsu/lab_2/test_lab_2.py
@@ -8,23 +8,22 @@ import lab_2_def as iam
 
 NewUser = 'jhsu-lab-2-boto3-user2'
 
-
 @mock_iam
 def test_mock_create_use():
    iam_client   = boto3.client('iam')
-   UserName = iam.iam_create_user(iam_client, NewUser)
-   print(UserName)
-   assert NewUser == UserName
+   iam_create_user_response = iam.iam_create_user(iam_client, NewUser)
+   print(iam_create_user_response)
+   assert NewUser == iam_create_user_response['User']['UserName']
 #  assert False
+
 
 @mock_iam
 def test_mock_delete_use():
    iam_client   = boto3.client('iam')
    UserName = iam.iam_create_user(iam_client, NewUser)
    iam_delete_user_response = iam.iam_delete_user(iam_client, NewUser)
-   print(iam_delete_user_response)
-   assert iam_delete_user_response['ResponseMetadata']['HTTPStatusCode'] == 200
-#  assert False
+   assert iam_delete_user_response is None
+
 
 
 
