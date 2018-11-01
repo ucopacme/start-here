f46e8da77c6d5dd1f1d83d4f1e390f8c2a9cd197
David Rivera <David.Rivera@ucop.edu>
2018-10-31 21:07:53 -0700

    updated test-lab1-add.py

--- hands_on_lab/user_submissions/davidjrivera/lab1/test-lab1-add.py
+++ hands_on_lab/user_submissions/davidjrivera/lab1/test-lab1-add.py
@@ -34,8 +34,7 @@ def test_dels3():
       s3 = boto3.client('s3')
       new_bucket = 'lab1davidpythons3'
       response =  s3.create_bucket( Bucket=new_bucket,  CreateBucketConfiguration={'LocationConstraint': 'us-west-2'})
-      delbuck = s3.delete_bucket(Bucket=new_bucket)
-      assert response['ResponseMetadata']['HTTPStatusCode'] == 404
-   #   print(delbuck)
-      #return delbuck
+      delbuck = s3.delete_bucket(Bucket='lab1davidpythons3')
+      print(delbuck)
+      assert delbuck['ResponseMetadata']['HTTPStatusCode'] == 204
 
